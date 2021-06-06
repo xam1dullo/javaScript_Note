bd7a17b07010e6cfa7c435d6fa66f14d81bc5765
xam1dullo <khkhamidullo@gmail.com>
2021-06-06 19:36:57 +0500

    add index and js file js note

---
+++ note/theFirstDay.js
@@ -0,0 +1,27 @@
+/** */
+// 1. NAN --> Not a Number -raqam yoki son emas string yoki son, raqam ustida bajarilganda kelib chiqadi
+// 2. null --> bo'shliq, bo'sh - topilmagan
+// 3. undifined -->ta'rifi berilmagan, brauzerda bu haqida ma'lumot yo'q - e'lon qilinmagan
+
+
+// kabeb -case -format --> kabob false;
+// snake_case_format --> ilon true;
+// xamelCaseFormat-- > tuya true;
+
+// var firstName = 'xamidullo';
+// var lastName = 'Xuodyberdiyev';
+// var dadName = 'Ulug\'bek o\'g\'li';
+
+// console.log(`salom ${firstName} ${lastName} ${dadName}`);
+
+
+let firstName = prompt('Ismingiz');
+let lastName = prompt('Familiyangiz:');
+let age = prompt('Yoshingiz:');
+let movies = prompt('yoqtirgan kinoyingiz:');
+Math.pow(2, 6); //darsja
+Math.sqrt(2, 6); //ildiz
+Math.cbrt(2, 6); // kub ildiz
+Math.random(2, 6); // random 
+Math.floor(23.6); // butun qism oldingo return 23 
+Math.ceil(23.1); // butun qism  orqaga return 24
